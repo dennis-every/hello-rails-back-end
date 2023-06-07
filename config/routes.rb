9d995069ef1b414668ec87032309ef140301b315
Dennis Every <dennis@ansyra.com>
2023-06-07 08:29:44 -0400

    Config rack-cors, set routes defaults to json

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   namespace 'api' do
     namespace 'v1' do
-      resources :messages, only: :index
+      resources :messages, only: :index, defaults: { format: :json }
     end
   end
 end
